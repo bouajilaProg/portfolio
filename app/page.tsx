774fe646da56250b7b1c242ce5f7fef633ed15c3
bouajilaprog <bouajilamedyessine@gmail.com>
2025-02-07 17:02:05 +0100

    removed the hover effect in education certif part + uploaded cv

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-import Navbar from "../components/Navbar"
 import Hero from "../components/Hero"
 import Services from "../components/Services"
 import Projects from "../components/Projects"
 import Education from "../components/Education"
-import Contact from "../components/Contact"
 
 export default function Home() {
   return (
--- components/Education.tsx
+++ components/Education.tsx
@@ -34,7 +34,6 @@ export default function Education() {
                   initial={{ opacity: 0, y: 50 }}
                   animate={{ opacity: 1, y: 0 }}
                   transition={{ duration: 0.5, delay: index * 0.1 }}
-                  whileHover={{ scale: 1.05 }}
                 >
                   <h4 className="text-lg font-semibold mb-2">{cert.name}</h4>
                   <p className="text-gray-400">{cert.issuer}</p>
