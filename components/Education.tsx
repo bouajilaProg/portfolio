9bf9e3dd5bbe0fd54e4045bcb131189e22788dc8
bouajilaprog <bouajilamedyessine@gmail.com>
2025-09-03 12:20:35 +0100

    style(education): made the courses into 3 cols grid

--- components/Education.tsx
+++ components/Education.tsx
@@ -107,7 +107,7 @@ export default function Education() {
             </motion.div>
 
             <motion.div
-              className="grid grid-cols-1 md:grid-cols-2 gap-6"
+              className="grid grid-cols-1 md:grid-cols-3 gap-6"
               initial={{ opacity: 0, y: 50 }}
               animate={{ opacity: 1, y: 0 }}
               transition={{ duration: 0.5, delay: 0.4 }}
